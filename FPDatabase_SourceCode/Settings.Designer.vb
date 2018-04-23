c833ae4e879746a4fd94690e788482a6e4d61ce3
GitEdvard <edvard.englund@medsci.uu.se>
2018-04-23 15:42:36 +0200

    Move settings.settings to project root
    Hopefully the app.config file will now work in deploy catalog.

--- FPDatabase_SourceCode/Settings.Designer.vb
+++ FPDatabase_SourceCode/Settings.Designer.vb
@@ -10,16 +10,16 @@
 
 Option Strict On
 Option Explicit On
-Namespace Properties
 
 
-    <Global.System.Runtime.CompilerServices.CompilerGeneratedAttribute(),  _
-        Global.System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "14.0.0.0"),  _
-        Global.System.ComponentModel.EditorBrowsableAttribute(Global.System.ComponentModel.EditorBrowsableState.Advanced)>  _
-    Partial Friend NotInheritable Class Settings
-        Inherits Global.System.Configuration.ApplicationSettingsBase
+
+<Global.System.Runtime.CompilerServices.CompilerGeneratedAttribute(),  _
+ Global.System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "14.0.0.0"),  _
+ Global.System.ComponentModel.EditorBrowsableAttribute(Global.System.ComponentModel.EditorBrowsableState.Advanced)>  _
+Partial Friend NotInheritable Class Settings
+    Inherits Global.System.Configuration.ApplicationSettingsBase
     
-        Private Shared defaultInstance As Settings = CType(Global.System.Configuration.ApplicationSettingsBase.Synchronized(New Settings()),Settings)
+    Private Shared defaultInstance As Settings = CType(Global.System.Configuration.ApplicationSettingsBase.Synchronized(New Settings()),Settings)
     
 #Region "My.Settings Auto-Save Functionality"
 #If _MyType = "WindowsForms" Then
@@ -36,8 +36,8 @@ Namespace Properties
 #End If
 #End Region
     
-        Public Shared ReadOnly Property [Default]() As Settings
-            Get
+    Public Shared ReadOnly Property [Default]() As Settings
+        Get
             
 #If _MyType = "WindowsForms" Then
                If Not addedHandler Then
@@ -49,31 +49,32 @@ Namespace Properties
                     End SyncLock
                 End If
 #End If
-                Return defaultInstance
-            End Get
-        End Property
+            Return defaultInstance
+        End Get
+    End Property
     
-        <Global.System.Configuration.ApplicationScopedSettingAttribute(),  _
-            Global.System.Diagnostics.DebuggerNonUserCodeAttribute(),  _
-            Global.System.Configuration.DefaultSettingValueAttribute("False")>  _
-        Public ReadOnly Property EnforceAppVersion() As Boolean
-            Get
-                Return CType(Me("EnforceAppVersion"),Boolean)
-            End Get
-        End Property
-    End Class
+    <Global.System.Configuration.ApplicationScopedSettingAttribute(),  _
+     Global.System.Diagnostics.DebuggerNonUserCodeAttribute(),  _
+     Global.System.Configuration.DefaultSettingValueAttribute("False")>  _
+    Public ReadOnly Property EnforceAppVersion() As Boolean
+        Get
+            Return CType(Me("EnforceAppVersion"),Boolean)
+        End Get
+    End Property
+End Class
 
+Namespace My
     
     <Global.Microsoft.VisualBasic.HideModuleNameAttribute(),  _
-        Global.System.Diagnostics.DebuggerNonUserCodeAttribute(),  _
-        Global.System.Runtime.CompilerServices.CompilerGeneratedAttribute()>  _
+     Global.System.Diagnostics.DebuggerNonUserCodeAttribute(),  _
+     Global.System.Runtime.CompilerServices.CompilerGeneratedAttribute()>  _
     Friend Module MySettingsProperty
         
         <Global.System.ComponentModel.Design.HelpKeywordAttribute("My.Settings")>  _
-        Friend ReadOnly Property Settings() As Global.FPDatabase.Properties.Settings
+        Friend ReadOnly Property Settings() As Global.FPDatabase.Settings
             Get
-                Return Global.FPDatabase.Properties.Settings.Default
+                Return Global.FPDatabase.Settings.Default
             End Get
         End Property
     End Module
-End NameSpace
+End Namespace
